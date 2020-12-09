0598ad4b74a4bd991485fb92cc475c2529e03e8c
Saurav Kumar <56781693+SauravKumar-max@users.noreply.github.com>
2020-12-09 17:45:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default function App() {
       <h1>EmojiPedia</h1>
       <input
         className="inputEl"
-        placeholder="Enter your flag emoji..."
+        placeholder="Enter your emoji..."
         onChange={emojiInputHandler}
       />
       <p className="meaning"> {meaning} </p>
